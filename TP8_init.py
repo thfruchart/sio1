aa07ad64e0540b4e59003c0dc92f57d4c5f4716a
thfruchart <66477650+thfruchart@users.noreply.github.com>
2024-01-12 09:32:57 +0100

    Add files via upload

---
+++ TP8_init.py
@@ -0,0 +1,16 @@
+﻿# TP8
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+a = 5
+b = 17
+
+def RANG(car):
+    resu = -1
+    i = 0
+    while (i<26) and resu == -1:
+        if (car ==ALPHABET[i]):
+            resu = i
+        # if_end
+        i = i+1
+    #for_end
+    return resu

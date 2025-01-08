f6a712da79cb1e3332419f7332c22e40296d09ea
thfruchart <66477650+thfruchart@users.noreply.github.com>
2025-01-08 16:11:10 +0100

    Add files via upload

---
+++ chiffrement_affine.py
@@ -0,0 +1,10 @@
+﻿ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def RANG(car) :
+    resu = -1
+    i = 0
+    while (i<26) and (resu == -1) :
+        if (car == ALPHABET[i]) :
+            resu = i
+        i = i+1
+    return resu
